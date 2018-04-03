7219722c4ad89aeb15abcb7979c0803b9ac59f70
Said <saidsl@users.noreply.github.com>
2018-04-03 10:37:17 +0800

    Initial commit

---
+++ bank_balance/bank_balance/doctype/daily_bank_balance/daily_bank_balance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Said Salameh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Daily Bank Balance', {
+	refresh: function(frm) {
+
+	}
+});
